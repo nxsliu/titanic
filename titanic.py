baa9cba14b90d5c1490063b6acb1ab5b2cfcafea
Nathan Liu <nxsliu@gmail.com>
2018-03-23 13:03:50 +0000

    data analysis and manipulation

--- titanic.py
+++ titanic.py
@@ -38,12 +38,126 @@ combine = [train_df, test_df]
 # print("-- Describe--")
 # print(train_df.describe(include=['O']).to_string())
 
-print("-- Data Analysis --")
-print("-- Pclass --")
-print(train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False))
-print("-- Sex --")
-print(train_df[['Sex', 'Survived']].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False))
-print("-- SibSp --")
-print(train_df[['SibSp', 'Survived']].groupby(['SibSp'], as_index=False).mean().sort_values(by='Survived', ascending=False))
-print("-- Parch --")
-print(train_df[['Parch', 'Survived']].groupby(['Parch'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+# print("-- Data Analysis --")
+# print("-- Pclass --")
+# print(train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+# print("-- Sex --")
+# print(train_df[['Sex', 'Survived']].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+# print("-- SibSp --")
+# print(train_df[['SibSp', 'Survived']].groupby(['SibSp'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+# print("-- Parch --")
+# print(train_df[['Parch', 'Survived']].groupby(['Parch'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+
+# g = sns.FacetGrid(train_df, col='Survived')
+# g.map(plt.hist, 'Age', bins=20)
+# plt.show()
+
+# grid = sns.FacetGrid(train_df, col='Pclass', hue='Survived')
+# #grid = sns.FacetGrid(train_df, col='Survived', row='Pclass', size=2.2, aspect=1.6)
+# grid.map(plt.hist, 'Age', alpha=.5, bins=20)
+# grid.add_legend()
+# plt.show()
+
+# #grid = sns.FacetGrid(train_df, col='Embarked')
+# grid = sns.FacetGrid(train_df, row='Embarked', size=2.2, aspect=1.6)
+# grid.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', palette='deep')
+# grid.add_legend()
+# plt.show()
+
+# #grid = sns.FacetGrid(train_df, row='Embarked', hue='Survived', palette={0: 'k', 1: 'w'})
+# grid = sns.FacetGrid(train_df, row='Embarked', col='Survived', size=2.2, aspect=1.6)
+# grid.map(sns.barplot, 'Sex', 'Fare', alpha=.5, ci=None)
+# grid.add_legend()
+# plt.show()
+
+# Wrangle data
+
+#print("Before", train_df.shape, test_df.shape, combine[0].shape, combine[1].shape)
+
+train_df = train_df.drop(['Ticket', 'Cabin'], axis=1)
+test_df = test_df.drop(['Ticket', 'Cabin'], axis=1)
+
+combine = [train_df, test_df]
+
+#print("After", train_df.shape, test_df.shape, combine[0].shape, combine[1].shape)
+
+# extract title from name
+for dataset in combine:
+    dataset['Title'] = dataset.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+
+#print(pd.crosstab(train_df['Title'], train_df['Sex']))
+
+# consolidate titles
+for dataset in combine:
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess', 'Capt', 'Col', 'Don', \
+    'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+
+#print(train_df[['Title', 'Survived']].groupby(['Title'], as_index=False).mean())
+
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+for dataset in combine:
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
+
+#print(train_df.head().to_string())
+
+# drop columns
+train_df = train_df.drop(['PassengerId', 'Name'], axis=1)
+test_df = test_df.drop(['Name'], axis=1)
+combine = [train_df, test_df]
+#print(train_df.shape, test_df.shape)
+
+# converting a categorical feature
+for dataset in combine:
+    dataset['Sex'] = dataset['Sex'].map({'female': 1, 'male': 0}).astype(int)
+
+#print(train_df.head().to_string())
+
+# #grid = sns.FacetGrid(train_df, col='Pclass', hue='Sex')
+# grid = sns.FacetGrid(train_df, row='Pclass', col='Sex', size=2.2, aspect=1.6)
+# grid.map(plt.hist, 'Age', alpha=.5, bins=20)
+# grid.add_legend()
+# plt.show()
+
+# complete a numerical and continuous feature - age
+guess_ages = np.zeros((2,3))
+#print(guess_ages)
+
+for dataset in combine:
+    for i in range(0, 2):
+        for j in range(0,3):
+            guess_df = dataset[(dataset['Sex'] == i) & (dataset['Pclass'] == j+1)]['Age'].dropna()
+
+            age_guess = guess_df.median()
+
+            guess_ages[i,j] = int(age_guess/0.5 + 0.5) * 0.5
+
+    for i in range(0,2):
+        for j in range(0,3):
+            #print(dataset.loc[(dataset.Age.isnull()) & (dataset.Sex == i) & (dataset.Pclass == j+1)].to_string())
+            dataset.loc[(dataset.Age.isnull()) & (dataset.Sex == i) & (dataset.Pclass == j+1), 'Age'] = guess_ages[i,j]
+
+    dataset['Age'] = dataset['Age'].astype(int)
+
+#print(train_df.head().to_string())
+
+train_df['AgeBand'] = pd.cut(train_df['Age'], 5)
+#print(train_df[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=False).mean().sort_values(by='AgeBand', ascending=True))
+
+for dataset in combine:
+    dataset.loc[dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[dataset['Age'] > 64, 'Age'] = 4
+
+#print(train_df.head().to_string())    
+
+train_df = train_df.drop(['AgeBand'], axis=1)
+combine = [train_df, test_df]
+#print(train_df.head().to_string())
+
